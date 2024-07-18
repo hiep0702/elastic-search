2dbb1bff2c3a2e55de3d35719802eb0ca07fe01a
hiep0702 <trantuanhiep@gmail.com>
2024-07-18 14:45:55 +0700

    laravel scout

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,28 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Employee;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $employee = Employee::all();
+
+        return view('welcome', compact('employee'));
+    }
+
+    public function search()
+    {
+        $employee = Employee::search(request('key_search'))->get();
+
+        return view('welcome', compact('employee'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/list-users', [Controller::class, 'index']);
+Route::get('/search', [Controller::class, 'search'])->name('search');
